44399af7d8b541bf0b0d828678e14597a3dc467c
Rails Course <admin@pmplinks.com>
2016-02-01 01:51:55 +0000

    adding stock prices method

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,7 @@
+class UserController < ApplicationController
+   
+   def my_portfolio
+       
+   end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-root 'welcome#index'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'users#my_portfolio'
+  get 'my_portfolio', to: 'users#my_portfolio'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
