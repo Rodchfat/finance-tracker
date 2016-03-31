d7ca8aabebfa1daa384aa95afec6ca919b6e6a93
Rod c <raadce@gmail.com>
2016-03-31 21:09:33 +0000

    adding first name and last name to user registration form

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -61,7 +61,7 @@ class UserStocksController < ApplicationController
     
     if @user_stock.save
     
-    format.html { redirect_to root_path, notice: "Stock #{@user_stock.stock.ticker} stock was successfully added" }
+    format.html { redirect_to my_portfolio_path, notice: "Stock #{@user_stock.stock.ticker} stock was successfully added" }
     
     format.json { render :show, status: :created, location: @user_stock }
     
@@ -96,7 +96,7 @@ class UserStocksController < ApplicationController
   def destroy
     @user_stock.destroy
     respond_to do |format|
-      format.html { redirect_to root_path, notice: 'User stock was successfully destroyed.' }
+      format.html { redirect_to my_portfolio_path, notice: 'User stock was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
