e84d7ce3a29611bea9b8f3387a00aa24a77a4ade
Rod c <raadce@gmail.com>
2016-04-01 19:51:34 +0000

    add destroy friendship and destroy page

--- db/migrate/20160401144254_create_friendships.rb
+++ db/migrate/20160401144254_create_friendships.rb
@@ -2,7 +2,6 @@ class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
       t.belongs_to :user
-
       t.belongs_to :friend, class: 'User'
 
       t.timestamps null: false
